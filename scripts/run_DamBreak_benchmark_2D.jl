4e843efe9532848c63b16d01af221f18c6921e84
Oriol Colomes <oriol.colomes@gmail.com>
2024-09-07 14:33:44 +0200

    2D benchmark run file

---
+++ scripts/run_DamBreak_benchmark_2D.jl
@@ -0,0 +1,56 @@
+using DrWatson
+@quickactivate "DamBreakSW"
+using TimerOutputs
+
+include(srcdir("DamBreakSW.jl"))
+
+# Warm-up run
+params_coarse_2D = DamBreakSW.DamBreak_benchmark_2D_params(
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;ρ∞=0.0)
+)
+params_coarse_2D_ASGS = DamBreakSW.DamBreak_benchmark_2D_params(
+  formulation=:ASGS,
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;ρ∞=0.0)
+)
+params_coarse_2D_Smagorinsky = DamBreakSW.DamBreak_benchmark_2D_params(
+  formulation=:Smagorinsky,
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;ρ∞=0.0)
+)
+DamBreakSW.main(params_coarse_2D)
+DamBreakSW.main(params_coarse_2D_ASGS)
+DamBreakSW.main(params_coarse_2D_Smagorinsky)
+
+to = TimerOutput()
+
+# Fine run
+params_fine_2D = DamBreakSW.DamBreak_benchmark_2D_params(
+  mesh_file=datadir("meshes","DamBreak_benchmark_2D_fine.msh"),
+  order=2,
+  verbose=false,
+  vtk_output=true,
+  vtk_folder="DamBreak_benchmark_2D",
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;T=10.0,ρ∞=0.0)
+)
+params_fine_2D_ASGS = DamBreakSW.DamBreak_benchmark_2D_params(
+  mesh_file=datadir("meshes","DamBreak_benchmark_2D_fine.msh"),
+  order=2,
+  formulation=:ASGS,
+  verbose=false,
+  vtk_output=true,
+  vtk_folder="DamBreak_benchmark_2D_ASGS",
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;T=10.0,ρ∞=0.0)
+)
+params_fine_2D_Smagorinsky = DamBreakSW.DamBreak_benchmark_2D_params(
+  mesh_file=datadir("meshes","DamBreak_benchmark_2D_fine.msh"),
+  order=2,
+  formulation=:Smagorinsky,
+  verbose=false,
+  vtk_output=true,
+  vtk_folder="DamBreak_benchmark_2D_Smagorinsky",
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;T=10.0,ρ∞=0.0)
+)
+@timeit to "DamBreak_benchmark_2D" DamBreakSW.main(params_fine_2D)
+# @timeit to "DamBreak_benchmark_2D_ASGS" DamBreakSW.main(params_fine_2D_ASGS)
+@timeit to "DamBreak_benchmark_2D_Smagorinsky" DamBreakSW.main(params_fine_2D_Smagorinsky)
+
+show(to)
