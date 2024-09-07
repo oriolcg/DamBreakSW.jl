b2c93817f6c68621983f1b57eb40ddabf3b5eedc
Oriol Colomes <oriol.colomes@gmail.com>
2024-09-07 14:33:29 +0200

    1D benchmark run file

--- scripts/run_DamBreak_benchmark_1D.jl
+++ scripts/run_DamBreak_benchmark_1D.jl
@@ -1,19 +1,62 @@
 using DrWatson
 @quickactivate "DamBreakSW"
+using TimerOutputs
 
 include(srcdir("DamBreakSW.jl"))
 
 # Warm-up run
-params_1D = DamBreakSW.DamBreak_benchmark_1D_params()
-DamBreakSW.main(params_1D)
+params_coarse_1D = DamBreakSW.DamBreak_benchmark_1D_params(
+  order=2,
+  verbose=false,
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;ρ∞=0.0)
+)
+params_coarse_1D_ASGS = DamBreakSW.DamBreak_benchmark_1D_params(
+  order=2,
+  formulation=:ASGS,
+  verbose=false,
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;ρ∞=0.0)
+)
+params_coarse_1D_ASGS = DamBreakSW.DamBreak_benchmark_1D_params(
+  order=2,
+  formulation=:Smagorinsky,
+  verbose=false,
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;ρ∞=0.0)
+)
+DamBreakSW.main(params_coarse_1D)
+DamBreakSW.main(params_coarse_1D_ASGS)
+DamBreakSW.main(params_coarse_1D_Smagorinsky)
+
+to = TimerOutput()
 
 # Fine run
 params_1D = DamBreakSW.DamBreak_benchmark_1D_params(
   nₓ=400,
   order=2,
-  T = 60.0,
   verbose=false,
   vtk_output=true,
-  vtk_folder="DamBreak_benchmark_1D"
+  vtk_folder="DamBreak_benchmark_1D",
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;T=60.0,ρ∞=0.0)
 )
-DamBreakSW.main(params_1D)
+params_1D_ASGS = DamBreakSW.DamBreak_benchmark_1D_params(
+  nₓ=400,
+  order=2,
+  formulation=:ASGS,
+  verbose=false,
+  vtk_output=true,
+  vtk_folder="DamBreak_benchmark_1D_ASGS",
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;T=60.0,ρ∞=0.0)
+)
+params_1D_Smagorinsky = DamBreakSW.DamBreak_benchmark_1D_params(
+  nₓ=400,
+  order=2,
+  formulation=:Smagorinsky,
+  verbose=false,
+  vtk_output=true,
+  vtk_folder="DamBreak_benchmark_1D_Smagorinsky",
+  ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α;T=60.0,ρ∞=0.0)
+)
+@timeit to "DamBreak_benchmark_1D" DamBreakSW.main(params_1D)
+@timeit to "DamBreak_benchmark_1D_ASGS" DamBreakSW.main(params_1D_ASGS)
+@timeit to "DamBreak_benchmark_1D_Smagorinsky" DamBreakSW.main(params_1D_Smagorinsky)
+
+show(to)
