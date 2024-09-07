8289b9ed4c1aac6ffe529de857d5957494e4143c
Oriol Colomes <oriol.colomes@gmail.com>
2024-09-07 14:33:13 +0200

    Updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,74 @@
 using DrWatson, Test
 @quickactivate "DamBreakSW.jl"
 using TimerOutputs
+using DataStructures
 
 include(srcdir("DamBreakSW.jl"))
+
+# Set up test parameters
+param_dict = OrderedDict(
+  :coarse_1D => DamBreakSW.DamBreak_benchmark_1D_params(),
+  :coarse_1D_Gα => DamBreakSW.DamBreak_benchmark_1D_params(
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  ),
+  :coarse_1D_Gα_ASGS => DamBreakSW.DamBreak_benchmark_1D_params(
+    formulation=:ASGS,
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  ),
+  :coarse_1D_Gα_Smagorinsky => DamBreakSW.DamBreak_benchmark_1D_params(
+    formulation=:Smagorinsky,
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  ),
+  :fine_1D => DamBreakSW.DamBreak_benchmark_1D_params(
+    nₓ=400,
+    verbose=true
+  ),
+  :fine_1D_Gα => DamBreakSW.DamBreak_benchmark_1D_params(
+    nₓ=400,
+    verbose=true,
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  ),
+  :fine_1D_Gα_ASGS => DamBreakSW.DamBreak_benchmark_1D_params(
+    nₓ=400,
+    verbose=true,
+    formulation=:ASGS,
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  ),
+  :fine_1D_Gα_Smagorinsky => DamBreakSW.DamBreak_benchmark_1D_params(
+    nₓ=400,
+    verbose=true,
+    formulation=:Smagorinsky,
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  ),
+  :coarse_2D => DamBreakSW.DamBreak_benchmark_2D_params(),
+  :fine_2D => DamBreakSW.DamBreak_benchmark_2D_params(
+    mesh_file = datadir("meshes","DamBreak_benchmark_2D_fine.msh"),
+    verbose=true
+  ),
+  :coarse_2D_Gα_ASGS => DamBreakSW.DamBreak_benchmark_2D_params(
+    formulation=:ASGS,
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  ),
+  :coarse_2D_Gα_Smagorinsky => DamBreakSW.DamBreak_benchmark_2D_params(
+    formulation=:Smagorinsky,
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  ),
+  :fine_2D_Gα_ASGS => DamBreakSW.DamBreak_benchmark_2D_params(
+    mesh_file = datadir("meshes","DamBreak_benchmark_2D_fine.msh"),
+    verbose=true,
+    formulation=:ASGS,
+    ode_solver_params=DamBreakSW.ODE_solver_params(:Generalized_α)
+  )
+)
 
 # Run test suite
 println("Starting tests")
 to = TimerOutput()
-
 @testset "DamBreakSW.jl tests" begin
-  params_1D = DamBreakSW.DamBreak_benchmark_1D_params()
-  @timeit to "Warm-up 1D test" begin
-    @test DamBreakSW.main(params_1D) == nothing
-  end
-  params_1D = DamBreakSW.DamBreak_benchmark_1D_params(
-    nₓ=400,
-    verbose=true
-  )
-  @timeit to "Fine 1D test" begin
-    @test DamBreakSW.main(params_1D) == nothing
+  for (key,value) in param_dict
+    @timeit to "$key test" begin
+      @test DamBreakSW.main(value) == nothing
+    end
   end
 end
 
